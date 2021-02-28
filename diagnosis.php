f8f958593ca7eae3ee0208a85499afe7dea57c12
Dosunmu Afeez <dosunmuafeez37@gmail.com>
2021-02-28 19:08:59 +0100

    initial commit

---
+++ diagnosis.php
@@ -0,0 +1,75 @@
+<?php
+include "includes/header.php";
+?>
+<div class="container-fluid mt-5 mb-5">
+	<div class="container pt-5">
+		<div class="row">
+			<div class="col-md-12">
+				<h4>Diagnosis</h4>
+				<table class="table table-bordered table-striped js-dataTable-full" style="width: 100%">
+					<thead>
+						<tr>
+							<th>S/N</th>
+							<th>Questions</th>
+							<th>Responses</th>
+							<th>Action</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>1</td>
+							<td>Are your crops getting black?</td>
+							<td>No</td>
+							<td><button class="btn btn-info rounded-0 btn-sm">View</button></td>
+						</tr>
+						<tr>
+							<td>2</td>
+							<td>Are your crops getting black?</td>
+							<td>No</td>
+							<td><button class="btn btn-info rounded-0 btn-sm">View</button></td>
+						</tr>
+						<tr>
+							<td>3</td>
+							<td>Are your crops getting black?</td>
+							<td>No</td>
+							<td><button class="btn btn-info rounded-0 btn-sm">View</button></td>
+						</tr>
+						<tr>
+							<td>4</td>
+							<td>Are your crops getting black?</td>
+							<td>No</td>
+							<td><button class="btn btn-info rounded-0 btn-sm">View</button></td>
+						</tr>
+						<tr>
+							<td>5</td>
+							<td>Are your crops getting black?</td>
+							<td>No</td>
+							<td><button class="btn btn-info rounded-0 btn-sm">View</button></td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+			<div class="row mb-5 mt-3">
+				<div class="col-md-12">
+					<h5 class="mt-4">Recommendation</h5>
+				</div>
+				<div class="col-md-8">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				</div>
+				<div class="col-md-4">
+					<img src="images/recommend.jpeg" class="img-fluid">
+				</div>
+			</div>
+				
+			</div>
+		</div>
+	</div>
+</div>
+<?php
+include "includes/footer.php";
+?>
