c1bc0e3be978cde6c5339e453ed8d93431dc410e
Dosunmu Afeez <dosunmuafeez37@gmail.com>
2021-03-26 21:10:01 +0100

    new

--- index.php
+++ index.php
@@ -1,39 +1,47 @@
-<?php
-include "includes/header.php";
-?>
-<div class="container-fluid main d-flex align-items-center">
-	<div class="container">
-		<div class="row">
-			<div class="col-md-7 d-flex align-self-center flex-column">
-				<h1 class="text-white text-uppercase font-weight-bold">Crop expert system</h1>
-				<p class="text-white">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
-			</div>
-			<div class="col-md-1"></div>
-			<div class="col-md-4">
-				<div class="card">
-					<div class="card-body">
-						<h5 class="font-weight-bold text-uppercase text-center">Login</h5>
-						<hr>
-						<form method="post" action="blogs.php">
-							<div class="form-group">
-								<label class="font-weight-bold">Username</label>
-								<input type="text" name="" class="form-control rounded-0" placeholder="Enter Username">
-							</div>
-							<div class="form-group">
-								<label class="font-weight-bold">Password</label>
-								<input type="password" name="" class="form-control rounded-0" placeholder="Enter Password">
-							</div>
-							<div class="form-group">
-								<button class="btn btn-success btn-block rounded-0 font-weight-bold">Login</button>
-							</div>
-						</form>
-						<p class="text-center" style="font-size: 13px">Don't have an account yet?<a href="register.php" class="font-weight-bold text-dark"> Register Here</a></p>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Crop Expert System</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="css/all.css">
+</head>
+<body>
+	<div class="container-fluid main d-flex align-items-center">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-7 d-flex align-self-center flex-column">
+					<h1 class="text-white text-uppercase font-weight-bold">Crop expert system</h1>
+					<p class="text-white">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+				</div>
+				<div class="col-md-1"></div>
+				<div class="col-md-4">
+					<div class="card">
+						<div class="card-body">
+							<h5 class="font-weight-bold text-uppercase text-center">Login</h5>
+							<hr>
+							<form method="post" action="blogs.php">
+								<div class="form-group">
+									<label class="font-weight-bold">Username</label>
+									<input type="text" name="" class="form-control rounded-0" placeholder="Enter Username">
+								</div>
+								<div class="form-group">
+									<label class="font-weight-bold">Password</label>
+									<input type="password" name="" class="form-control rounded-0" placeholder="Enter Password">
+								</div>
+								<div class="form-group">
+									<button class="btn btn-success btn-block rounded-0 font-weight-bold">Login</button>
+								</div>
+							</form>
+							<p class="text-center" style="font-size: 13px">Don't have an account yet?<a href="register.php" class="font-weight-bold text-dark"> Register Here</a></p>
+						</div>
 					</div>
 				</div>
 			</div>
 		</div>
 	</div>
-</div>
 </body>
 </html>
